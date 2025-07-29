b3142edb1557fbc248c5828da2e1614142a26cde
PhemeloTsogang <2543064@students.wits.ac.za>
2025-07-29 13:00:25 +0200

    Changes

---
+++ GROUP11_PROJECT/Assets/test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Is it working now");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
