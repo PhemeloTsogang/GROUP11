0f77ea82c991698062bbf7e0a04de7ec2ba633ab
Lesibaaa <lmamabolo.2005@gmail.com>
2025-08-18 13:57:27 +0200

    References

--- GROUP11_PROJECT/Assets/Scripts/Dialogue/Dialogue.cs
+++ GROUP11_PROJECT/Assets/Scripts/Dialogue/Dialogue.cs
@@ -8,3 +8,11 @@ public class Dialogue
     [TextArea(3,10)]
     public string[] sentences; 
 }
+
+/*
+Title: How to make a Dialogue System in Unity
+Author: Brackeys
+Date: 15 August 2025
+Code version: 1
+Availability: https://www.youtube.com/watch?v=_nRzoTzeyxU&t=564s
+ */
--- GROUP11_PROJECT/Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ GROUP11_PROJECT/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -9,3 +9,11 @@ public class DialogueTrigger : MonoBehaviour
         FindFirstObjectByType<DialogueManager>().StartDialogue(dialogue);
     }
 }
+
+/*
+Title: How to make a Dialogue System in Unity
+Author: Brackeys
+Date: 15 August 2025
+Code version: 1
+Availability: https://www.youtube.com/watch?v=_nRzoTzeyxU&t=564s
+*/
