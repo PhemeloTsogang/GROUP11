7106cd5e84452498800505d575f73380bd5f151d
PhemeloTsogang <2543064@students.wits.ac.za>
2025-10-25 17:17:06 +0200

    level shenanigans

---
+++ GROUP11_PROJECT/Assets/Scripts/StartLevel2.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class StartLevel2 : MonoBehaviour
+{
+    public GameObject closeOff, player, levelText;
+    private AudioSource Drip;
+    public AudioManager manager;
+
+
+    private void Update()
+    {
+        if (levelText.activeInHierarchy)
+        {
+            StartCoroutine(wait());
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AudioManager.instance.StopSound(manager.Heart);
+            levelText.SetActive(true);
+            closeOff.SetActive(true);
+            if (Drip == null || !Drip.isPlaying)
+            {
+                Drip = AudioManager.instance.Play("Water", player.transform);
+            }
+        }
+    }
+
+    private IEnumerator wait()
+    {
+        yield return new WaitForSeconds(4f);
+        levelText.SetActive(false);
+    }
+}
