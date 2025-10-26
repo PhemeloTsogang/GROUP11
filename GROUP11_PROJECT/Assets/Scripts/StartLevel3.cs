9b68a224252260036d64a7d406488b1b02f85e89
PhemeloTsogang <2543064@students.wits.ac.za>
2025-10-26 17:04:28 +0200

    Level3 Updates

--- GROUP11_PROJECT/Assets/Scripts/StartLevel3.cs
+++ GROUP11_PROJECT/Assets/Scripts/StartLevel3.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 public class StartLevel2 : MonoBehaviour
 {
-    public GameObject closeOff, player, levelText;
+    public GameObject closeOff, player, levelText, monst1, monst2;
     private AudioSource Drip;
     public AudioManager manager;
+    public StartHeartbeat heartbeat;
     public FPController memory;
 
 
@@ -22,7 +23,22 @@ public class StartLevel2 : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            if (monst1.activeInHierarchy)
+            {
+                monst1.SetActive(false);
+            }
+
+            if (!monst2.activeInHierarchy)
+            {
+                monst2.SetActive(true);
+            }
+
             AudioManager.instance.StopSound(manager.Heart);
+            if (heartbeat.Heart != null)
+            {
+                AudioManager.instance.StopSound(heartbeat.Heart);
+            }
+
             levelText.SetActive(true);
             closeOff.SetActive(true);
             if (Drip == null || !Drip.isPlaying)
